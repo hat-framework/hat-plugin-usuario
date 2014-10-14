a0047059d55a9729287f260ffa78923a76ba499d
Paulo Veneroso <venerosobrasil@gmail.com>
2014-10-14 10:30:13 -0300

    loader com css

--- Config/usuarioLoader.php
+++ Config/usuarioLoader.php
@@ -6,6 +6,9 @@ class usuarioLoader extends classes\Classes\PluginLoader{
         $this->LoadComponent('usuario/login', 'uobj');
         $menu = $this->uobj->menu();
         if(!empty($menu)) $this->setVar("menu", $menu);
+        if (isset($_REQUEST['ajax']))return;
+        $this->LoadResource('html/html', 'html');
+        $this->html->LoadCss('modulos/usuario');
     }
     
     public function setAdminVars(){}
