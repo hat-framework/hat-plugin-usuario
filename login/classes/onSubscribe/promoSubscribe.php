8dc7ec5247ef8435bd03b4c680c42784ca191a40
tigredonorte <tigredonorte3@gmail.com>
2015-12-23 14:52:42 -0200

    codPromoChanges

--- login/classes/onSubscribe/promoSubscribe.php
+++ login/classes/onSubscribe/promoSubscribe.php
@@ -4,15 +4,15 @@ class referrerSubscribe extends classes\Classes\Object{
     public function execute($cod_usuario, $array){
         $codpromo = isset($array['promocod'])?$array['promocod']:"";
         $this->LoadModel('usuario/promocod', 'promo');
-        if($refer === ""){
-            $refer = $this->promo->getCookie();
-            if($refer === ""){return;}
+        if($codpromo === ""){
+            $codpromo = $this->promo->getCookie();
+            if($codpromo === ""){return;}
         }
         
         try{
             if(false === $this->LoadModel('usuario/promocod/promouser', 'pruser')->attachPromocod($codpromo, $cod_usuario)){return true;}
         } catch (Exception $ex) {
-            sendEmailToWebmasters('Erro ao associar promoção', "Ocorreu algum erro ao associar a promoção $refer para o usuário $cod_usuario");
+            sendEmailToWebmasters('Erro ao associar promoção', "Ocorreu algum erro ao associar a promoção $codpromo para o usuário $cod_usuario");
             return true;
         }
         
